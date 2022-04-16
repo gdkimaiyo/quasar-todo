b180e3e38fcd3e08aa17763a89feddc78f4a7e7b
gdkimaiyo <gdkimaiyo@gmail.com>
2022-04-16 23:42:42 +0300

    ch: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'initiate deployment'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gdkimaiyo/quasar-todo.git main:gh-pages
+git push -f git@github.com:gdkimaiyo/quasar-todo.git master:gh-pages
 
 cd -
